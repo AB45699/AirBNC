d77add3c549c49238a55363ea86bc8c3628fc51e
AB45699 <b.anam8541@gmail.com>
2025-09-19 23:23:36 +0100

    changed to ignore if is host is t or f

--- db/utils.js
+++ db/utils.js
@@ -1,14 +1,26 @@
-function createHostsRef(users) {
-    let hostsRefObj = {};
+function createUsersRef(users) {
+    let usersRefObj = {};
 
-    users.forEach(({first_name, surname, user_id, is_host}) =>{
-        if (is_host) {
-            const hostName = `${first_name} ${surname}`;
-            hostsRefObj[hostName] = user_id;
-        }
+    users.forEach(({first_name, surname, user_id }) =>{
+        const hostName = `${first_name} ${surname}`;
+        usersRefObj[hostName] = user_id;
     });
 
-    return hostsRefObj;
+    return usersRefObj;
 }
 
-module.exports = createHostsRef;
+function formatUsers(users, userRef) {
+    return users.map(({ name, property_type, location, price_per_night, description, host_name}) => [
+        
+        userRef[host_name], 
+        name,
+        location, 
+        property_type, 
+        price_per_night, 
+        description
+
+        ]
+    )
+}
+
+module.exports = { createUsersRef, formatUsers };
--- db/utils.test.js
+++ db/utils.test.js
@@ -1,78 +1,133 @@
-const createHostsRef = require("./utils.js");
+const { createUsersRef, formatUsers } = require("./utils.js");
 
-describe("createHostsRef", () => {
+let testUserData;
+
+beforeEach(() => {
+  testUserData = [
+    {
+      name: "Modern Apartment in City Center",
+      property_type: "Apartment",
+      location: "London, UK",
+      price_per_night: 120.0,
+      description:
+        "Description of Modern Apartment in City Center. A sleek apartment with all modern amenities.",
+      host_name: "Alice Johnson",
+    },
+    {
+      name: "Cosy Family House",
+      property_type: "House",
+      location: "Manchester, UK",
+      price_per_night: 150.0,
+      description:
+        "A spacious home perfect for families looking to explore the city.",
+      host_name: "Emma Davis",
+    },
+  ];
+});
+
+describe("createUsersRef", () => {
   test("returns an object", () => {
-    expect(typeof createHostsRef([])).toBe("object");
-    expect(Array.isArray(createHostsRef([]))).toBe(false);
+    expect(typeof createUsersRef([])).toBe("object");
+    expect(Array.isArray(createUsersRef([]))).toBe(false);
   });
   test("an empty input array returns an empty object", () => {
-    expect(createHostsRef([])).toEqual({});
+    expect(createUsersRef([])).toEqual({});
   });
   test("assigns first_name and surname as the key on the ref object for one user", () => {
-    const user = [
-      { first_name: "Alice", surname: "Johnson", user_id: 1, is_host: true },
-    ];
-    const hostRef = createHostsRef(user);
+    const user = [{ first_name: "Alice", surname: "Johnson", user_id: 1 }];
+    const userRef = createUsersRef(user);
 
-    expect(hostRef).toHaveProperty("Alice Johnson");
+    expect(userRef).toHaveProperty("Alice Johnson");
   });
   test("assigns user_id as the value to the host name key for one user", () => {
-    const user = [
-      { first_name: "Alice", surname: "Johnson", user_id: 1, is_host: true },
-    ];
-    const hostRef = createHostsRef(user);
+    const user = [{ first_name: "Alice", surname: "Johnson", user_id: 1 }];
+    const userRef = createUsersRef(user);
 
-    expect(hostRef["Alice Johnson"]).toBe(1);
-  });
-  test("if is_host property is false, an empty object is returned, for one user", () => {
-    const user = [
-      { first_name: "Alice", surname: "Johnson", user_id: 1, is_host: false },
-    ];
-
-    expect(createHostsRef(user)).toEqual({});
+    expect(userRef["Alice Johnson"]).toBe(1);
   });
   test("assigns first_name and surname as the key on the ref object for multiple users", () => {
     const users = [
-      { first_name: "Alice", surname: "Johnson", user_id: 1, is_host: true },
-      { first_name: "Emma", surname: "Davis", user_id: 3, is_host: true },
+      { first_name: "Alice", surname: "Johnson", user_id: 1 },
+      { first_name: "Emma", surname: "Davis", user_id: 3 },
     ];
-    const hostRef = createHostsRef(users);
+    const usersRef = createUsersRef(users);
 
-    expect(hostRef).toHaveProperty("Alice Johnson");
-    expect(hostRef).toHaveProperty("Emma Davis");
+    expect(usersRef).toHaveProperty("Alice Johnson");
+    expect(usersRef).toHaveProperty("Emma Davis");
   });
   test("assigns user_id as the value to the host name key for multiple users", () => {
     const users = [
-      { first_name: "Alice", surname: "Johnson", user_id: 1, is_host: true },
-      { first_name: "Emma", surname: "Davis", user_id: 3, is_host: true },
+      { first_name: "Alice", surname: "Johnson", user_id: 1 },
+      { first_name: "Emma", surname: "Davis", user_id: 3 },
     ];
-    const hostRef = createHostsRef(users);
+    const usersRef = createUsersRef(users);
 
-    expect(hostRef["Alice Johnson"]).toBe(1);
-    expect(hostRef["Emma Davis"]).toBe(3);
-  });
-  test("with multiple users, only hosts are returned in the reference object", () => {
-    const users = [
-      { first_name: "Alice", surname: "Johnson", user_id: 1, is_host: true },
-      { first_name: "Bob", surname: "Smith", user_id: 2, is_host: false },
-      { first_name: "Emma", surname: "Davis", user_id: 3, is_host: true },
-    ];
-    const hostRef = createHostsRef(users);
-
-    expect(hostRef).toHaveProperty("Alice Johnson");
-    expect(hostRef).not.toHaveProperty("Bob Smith");
-    expect(hostRef).toHaveProperty("Emma Davis");
+    expect(usersRef["Alice Johnson"]).toBe(1);
+    expect(usersRef["Emma Davis"]).toBe(3);
   });
   test("the input array is not mutated", () => {
     const input = [
-      { first_name: "Alice", surname: "Johnson", user_id: 1, is_host: true },
-      { first_name: "Emma", surname: "Davis", user_id: 3, is_host: true },
+      { first_name: "Alice", surname: "Johnson", user_id: 1 },
+      { first_name: "Emma", surname: "Davis", user_id: 3 },
     ];
 
-    createHostsRef(input);
+    createUsersRef(input);
     expect(input).toEqual([
-      { first_name: "Alice", surname: "Johnson", user_id: 1, is_host: true },
-      { first_name: "Emma", surname: "Davis", user_id: 3, is_host: true },
+      { first_name: "Alice", surname: "Johnson", user_id: 1 },
+      { first_name: "Emma", surname: "Davis", user_id: 3 },
+    ]);
+  });
+});
+
+describe("formatUsers", () => {
+  test("returns an array", () => {
+    const userRef = createUsersRef([
+      { first_name: "Alice", surname: "Johnson", user_id: 1 },
+    ]);
+
+    expect(typeof formatUsers(testUserData, userRef)).toBe("object");
+    expect(Array.isArray(formatUsers([], {}))).toBe(true);
+  });
+  test("the returned formatted user has its id as the host_id for one user", () => {
+    const userRef = createUsersRef([
+      { first_name: "Alice", surname: "Johnson", user_id: 1 },
+    ]);
+
+    expect(formatUsers([testUserData[0]], userRef)).toEqual([
+      [
+        1,
+        "Modern Apartment in City Center",
+        "London, UK",
+        "Apartment",
+        120.0,
+        "Description of Modern Apartment in City Center. A sleek apartment with all modern amenities.",
+      ],
+    ]);
+  });
+  test("the returned formatted user has its id as the host_id for multiple users", () => {
+    const usersRef = createUsersRef([
+      { first_name: "Alice", surname: "Johnson", user_id: 1 },
+      { first_name: "Emma", surname: "Davis", user_id: 3 },
+    ]);
+
+    expect(formatUsers(testUserData, usersRef)).toEqual([
+      [
+        1,
+        "Modern Apartment in City Center",
+        "London, UK",
+        "Apartment",
+        120.0,
+        "Description of Modern Apartment in City Center. A sleek apartment with all modern amenities.",
+      ],
+      [
+        3, 
+        "Cosy Family House", 
+        "Manchester, UK", 
+        "House", 
+        150.0, 
+        "A spacious home perfect for families looking to explore the city.",
+
+      ]
     ]);
   });
 });
