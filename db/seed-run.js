d9128502de56a7e12a3b505f7b22ec2877fc4813
AB45699 <b.anam8541@gmail.com>
2025-09-23 20:15:57 +0100

    added db.end()

--- db/seed-run.js
+++ db/seed-run.js
@@ -1,4 +1,7 @@
 const seed = require("./seed");
+const db = require("./connection.js");
 const { propertyTypesData, usersData, propertiesData, reviewsData, imagesData, favouritesData, bookingsData } = require("./data/dev/index.js");
 
-seed(propertyTypesData, usersData, propertiesData, reviewsData, imagesData, favouritesData, bookingsData);
+seed(propertyTypesData, usersData, propertiesData, reviewsData, imagesData, favouritesData, bookingsData).then(()=>{
+    db.end();
+});
