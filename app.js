51cad392cd8f7f87030e7ab437be01b4b7752fd7
AB45699 <b.anam8541@gmail.com>
2025-10-20 21:24:25 +0100

    renamed error handling mwf to handle separated errors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express"); 
 const { getProperties, getPropertyById } = require("./controllers/properties");
 const { postPropertyReview, getPropertyReviews, deletePropertyReview } = require("./controllers/reviews.js");
-const { handlePathNotFound, handleBadRequests, handleServerErrors, handleCustomErrors } = require("./controllers/errors");
+const { handlePathNotFound, handleDataBaseErrors, handleServerErrors, handleCustomErrors } = require("./controllers/errors");
 const { getUserDetails } = require("./controllers/users.js");
 
 const app = express(); 
@@ -22,7 +22,7 @@ app.get("/api/users/:id", getUserDetails);
 
 app.all("/*path", handlePathNotFound);
 
-app.use(handleBadRequests);
+app.use(handleDataBaseErrors);
 
 app.use(handleCustomErrors); 
 
--- controllers/errors.js
+++ controllers/errors.js
@@ -3,26 +3,24 @@ exports.handlePathNotFound = (req, res, next) => {
     
 }; 
 
-exports.handleBadRequests = (err, req, res, next) => {
+exports.handleDataBaseErrors = (err, req, res, next) => {
     const codes = ["22P02", "23502", "23514"];
-    
-    if (codes.includes(err.code)) {
+
+    if (err.code === "23503") {
+        const item = err.constraint === "reviews_property_id_fkey" ? "Property" : "Guest";
+        res.status(404).send({msg: `${item} not found`});
+
+    } else if (codes.includes(err.code)) {
         res.status(400).send({msg: "Bad request"});
+
     } else {
         next(err);
-    }
+    };
+
 }; 
 
 exports.handleCustomErrors = (err, req, res, next) => {
-    if (err.code === "23503") {
-
-        if (err.constraint === "reviews_property_id_fkey") {
-            res.status(404).send({msg: "Property not found"});
-        } else {
-            res.status(404).send({msg: "Guest not found"});
-        }
-       
-    } else if (err.status) {
+    if (err.status) {
         res.status(err.status).send({msg: err.msg});
 
     } else {
