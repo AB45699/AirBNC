29369f8f22d6ed04efffa6d4a62e25db67ada8ad
AB45699 <b.anam8541@gmail.com>
2025-10-25 15:13:15 +0100

    removed console logs

--- controllers/errors.js
+++ controllers/errors.js
@@ -1,33 +1,28 @@
 exports.handlePathNotFound = (req, res, next) => {
-    res.status(404).send({msg: "Path not found"}); 
-    
-}; 
+  res.status(404).send({ msg: "Path not found" });
+};
 
 exports.handleDataBaseErrors = (err, req, res, next) => {
-    const codes = ["22P02", "23502", "23514"];
-    
-    if (err.code === "23503") {
-        const item = err.constraint === "reviews_property_id_fkey" ? "Property" : "Guest";
-        res.status(404).send({msg: `${item} not found`});
+  const codes = ["22P02", "23502", "23514"];
 
-    } else if (codes.includes(err.code)) {
-        res.status(400).send({msg: "Bad request"});
-
-    } else {
-        next(err);
-    };
-
-}; 
+  if (err.code === "23503") {
+    const item = err.constraint === "reviews_property_id_fkey" ? "Property" : "Guest";
+    res.status(404).send({ msg: `${item} not found` });
+  } else if (codes.includes(err.code)) {
+    res.status(400).send({ msg: "Bad request" });
+  } else {
+    next(err);
+  }
+};
 
 exports.handleCustomErrors = (err, req, res, next) => {
-    if (err.status) {
-        res.status(err.status).send({msg: err.msg});
-
-    } else {
-        next(err);
-    }
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
 };
 
 exports.handleServerErrors = (err, req, res, next) => {
-    res.status(500).send({msg: "Server error"});
-}; 
+  res.status(500).send({ msg: "Server error" });
+};
