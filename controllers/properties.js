fe4ed70a0c9acf31b41fac0fb8a23ca3e9cbd036
AB45699 <b.anam8541@gmail.com>
2025-10-25 15:33:26 +0100

    refactored

--- controllers/properties.js
+++ controllers/properties.js
@@ -1,17 +1,23 @@
-const { checkUserExists } = require("../models/checkUserExists");
-const { fetchProperties, fetchProperty } = require("../models/properties");
+const { checkUserExists } = require("../models/users.js");
+const { fetchProperties, fetchProperty } = require("../models/properties.js");
 const checkIfNumber = require("../db/utility-functions/checkIfNumber.js");
 const {checkPropertyTypeExists} = require("../models/propertyTypes.js");
 
 exports.getProperties = async (req, res, next) => {
     const { sort, order, maxprice, minprice, property_type } = req.query; 
-    
-    if (maxprice && !(checkIfNumber(maxprice)) || minprice && !(checkIfNumber(minprice))) {
+    const invalidMaxpriceQuery = maxprice && !(checkIfNumber(maxprice));
+    const invalidMinpriceQuery = minprice && !(checkIfNumber(minprice))
+
+    if (invalidMaxpriceQuery || invalidMinpriceQuery) {
         return Promise.reject({status: 400, msg: "Bad request"})  
     };
 
     if (property_type) {
-        await checkPropertyTypeExists(property_type);
+        const propertyType = await checkPropertyTypeExists(property_type);
+
+        if (propertyType === undefined) {
+            return Promise.reject({status: 404, msg: "Property type not found"});
+        }
     };
 
     const properties = await fetchProperties(sort, order, maxprice, minprice, property_type);
@@ -22,18 +28,20 @@ exports.getProperties = async (req, res, next) => {
 exports.getPropertyById = async (req, res, next) => {
     const { id } = req.params;
     const { user_id } = req.query;
+    const invalidUserID = user_id && !checkIfNumber(user_id);
+    const userNonExistent = user_id && ((await checkUserExists(user_id)) === undefined); 
 
-    if (!checkIfNumber(id) || user_id && !checkIfNumber(user_id)) {
+    if (!checkIfNumber(id) || invalidUserID) {
         return Promise.reject({status: 400, msg: "Bad request"});
-    }
+    };
 
-    if (user_id && !(await checkUserExists(user_id))) {
+    if (userNonExistent) {
         return Promise.reject({status: 404, msg: "User not found"})
     };
 
     const property = await fetchProperty(id, user_id);
     
-    if (!property) {
+    if (property === undefined) {
         return Promise.reject({status: 404, msg: "Property not found"}); 
     };
 
