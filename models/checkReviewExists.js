9be04b148cfe20655a611a6bea34ee4852837edc
Anam Baig <b.anam8541@gmail.com>
2025-10-26 15:58:52 +0000

    Delete models/checkReviewExists.js

--- models/checkReviewExists.js
+++
@@ -1,9 +0,0 @@
-const db = require("../db/connection.js");
-
-exports.checkReviewExists = async (reviewID) => {
-    const {rows: [review]} = await db.query(`SELECT review_id FROM reviews
-        WHERE review_id = $1
-        GROUP BY review_id;`, [reviewID]);
-    
-    return review
-};
