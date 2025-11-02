1fb3e0b961a6a9553f49dd506a3433e71775f189
AB45699 <b.anam8541@gmail.com>
2025-11-02 17:27:57 +0000

    promise.all for user exist check on get prop by ID

--- controllers/properties.js
+++ controllers/properties.js
@@ -12,16 +12,16 @@ exports.getProperties = async (req, res, next) => {
         return Promise.reject({status: 400, msg: "Bad request"})  
     };
 
-    const propertyPromises = [fetchProperties(sort, order, maxprice, minprice, property_type)];
+    const promises = [fetchProperties(sort, order, maxprice, minprice, property_type)];
 
     if (property_type) {
-        propertyPromises.push(checkPropertyTypeExists(property_type));
+        promises.push(checkPropertyTypeExists(property_type));
     };
 
-    const [properties, propertyType] = await Promise.all(propertyPromises);
+    const [properties, propertyType] = await Promise.all(promises);
 
     if (property_type && propertyType === undefined) {
-            return Promise.reject({status: 404, msg: "Property type not found"});
+        return Promise.reject({status: 404, msg: "Property type not found"});
     };
     
     res.status(200).send({ properties });
@@ -31,17 +31,22 @@ exports.getPropertyById = async (req, res, next) => {
     const { id } = req.params;
     const { user_id } = req.query;
     const invalidUserID = user_id && !checkIfNumber(user_id);
-    const userNonExistent = user_id && ((await checkUserExists(user_id)) === undefined); 
 
     if (!checkIfNumber(id) || invalidUserID) {
         return Promise.reject({status: 400, msg: "Bad request"});
     };
 
-    if (userNonExistent) {
-        return Promise.reject({status: 404, msg: "User not found"})
+    const promises = [fetchProperty(id, user_id)];
+    
+    if (user_id) {
+        promises.push(checkUserExists(user_id));
     };
 
-    const property = await fetchProperty(id, user_id);
+    const [property, user] = await Promise.all(promises);
+
+    if (user_id && user === undefined) {
+        return Promise.reject({status: 404, msg: "User not found"});
+    };
     
     if (property === undefined) {
         return Promise.reject({status: 404, msg: "Property not found"}); 
