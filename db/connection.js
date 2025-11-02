d3d2ae5a3123186f991c2f662f10fa082f793b0a
AB45699 <b.anam8541@gmail.com>
2025-11-02 21:17:33 +0000

    setting up hosting

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,17 @@
 const { Pool } = require("pg");
 
-const ENV = process.env.NODE_ENV;
+const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({path: `${__dirname}/../.env.${ENV}`});
 
-const pool = new Pool();
+const config = {};
+
+if (ENV === "production") {
+    config.connectionString = process.env.DATABASE_URL;
+    config.max = 2;
+};
+
+const pool = new Pool(config);
 
 module.exports = pool;
 
