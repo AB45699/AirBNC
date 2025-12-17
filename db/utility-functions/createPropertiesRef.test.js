fc32d4a61170e5cd7aa9adaea1dfd45039730aaa
AB45699 <b.anam8541@gmail.com>
2025-12-17 20:08:30 +0000

    refactoring

--- db/utility-functions/createPropertiesRef.test.js
+++ db/utility-functions/createPropertiesRef.test.js
@@ -1,5 +1,19 @@
 const createPropertiesRef = require("./createPropertiesRef.js");
 
+let propertyOne, propertyTwo; 
+
+beforeEach(()=>{
+  propertyOne = {
+    name: "Modern Apartment in City Center", 
+    property_id: 1 
+  };
+
+  propertyTwo = {
+    name: "Spacious Countryside House", 
+    property_id: 7
+  };
+
+})
 describe("createPropertiesRef", () => {
   test("returns an object", () => {
     expect(typeof createPropertiesRef([])).toBe("object");
@@ -9,37 +23,23 @@ describe("createPropertiesRef", () => {
     expect(createPropertiesRef([])).toEqual({});
   });
   test("assigns name (the property name) as the key on the ref object for one property", () => {
-    const property = [
-      { name: "Modern Apartment in City Center", property_id: 1 },
-    ];
-    const propertyRef = createPropertiesRef(property);
+    const propertyRef = createPropertiesRef([propertyOne]);
 
     expect(propertyRef).toHaveProperty("Modern Apartment in City Center");
   });
   test("assigns the property_id as the value to the property name key, for one property", () => {
-    const property = [
-      { name: "Modern Apartment in City Center", property_id: 1 },
-    ];
-    const propertyRef = createPropertiesRef(property);
+    const propertyRef = createPropertiesRef([propertyOne]);
 
     expect(propertyRef["Modern Apartment in City Center"]).toBe(1);
   });
   test("assigns name (the property name) as the key on the ref object for multiple properties", () => {
-    const properties = [
-      { name: "Modern Apartment in City Center", property_id: 1 },
-      { name: "Spacious Countryside House", property_id: 7 },
-    ];
-    const propertyRef = createPropertiesRef(properties);
+    const propertyRef = createPropertiesRef([propertyOne, propertyTwo]);
 
     expect(propertyRef).toHaveProperty("Modern Apartment in City Center");
     expect(propertyRef).toHaveProperty("Spacious Countryside House");
   });
   test("assigns the property_id as the value to the property name key, for multiple properties", () => {
-    const properties = [
-      { name: "Modern Apartment in City Center", property_id: 1 },
-      { name: "Spacious Countryside House", property_id: 7 },
-    ];
-    const propertyRef = createPropertiesRef(properties);
+    const propertyRef = createPropertiesRef([propertyOne, propertyTwo]);
 
     expect(propertyRef["Modern Apartment in City Center"]).toBe(1);
     expect(propertyRef["Spacious Countryside House"]).toBe(7);
