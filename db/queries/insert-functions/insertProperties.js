7b73b85e4d589b32fe99cf14bc96d0c5a8aa4c4e
AB45699 <b.anam8541@gmail.com>
2025-12-27 20:26:33 +0000

    refactoring code, adding edge case testing

--- db/queries/insert-functions/insertProperties.js
+++ db/queries/insert-functions/insertProperties.js
@@ -1,5 +1,9 @@
 function insertProperties(userRef, propertyData) {
-    return propertyData.map(({host_name, name, location, property_type, price_per_night, description}) => [
+    return propertyData
+        .filter(({host_name})=>
+            userRef[host_name] !== undefined
+        ) 
+        .map(({host_name, name, location, property_type, price_per_night, description}) => [
             userRef[host_name], 
             name, 
             location, 
--- db/queries/insert-functions/insertProperties.test.js
+++ db/queries/insert-functions/insertProperties.test.js
@@ -1,6 +1,6 @@
 const insertProperties = require("./insertProperties.js"); 
 
-let propertyData, multipleUsersRef;
+let propertyData, multipleUsersRef, expectedOutcome;
 
 beforeEach(()=>{
     propertyData = [
@@ -9,18 +9,18 @@ beforeEach(()=>{
             "property_type": "Apartment",
             "location": "London, UK",
             "price_per_night": 120.0,
-            "description": "Description of Modern Apartment in City Center. A sleek apartment with all modern amenities.",
+            "description": "Description of Modern Apartment in City Center.",
             "host_name": "Alice Johnson",
-            "amenities": ["WiFi", "TV", "Kitchen", "Washing Machine"]
+            "amenities": ["WiFi", "TV", "Kitchen"]
         },
         {
             "name": "Elegant City Apartment",
             "property_type": "Apartment",
             "location": "Birmingham, UK",
             "price_per_night": 110.0,
-            "description": "Stylish apartment located in the heart of Birmingham, close to all attractions.",
+            "description": "Description of Elegant City Apartment.",
             "host_name": "Emma Davis",
-            "amenities": ["WiFi", "TV", "Kitchen", "Washing Machine", "Iron"]
+            "amenities": ["TV", "Kitchen", "Washer"]
         }
     ];
 
@@ -29,21 +29,41 @@ beforeEach(()=>{
         "Emma Davis": 2
     };
 
+    expectedOutcome = [
+        [
+            2, 
+            "Elegant City Apartment", 
+            "Birmingham, UK", 
+            "Apartment", 
+            110.0, 
+            "Description of Elegant City Apartment."
+        ]
+    ];
+
 });
 
 describe("insertProperties", ()=>{
     test("returns an array", ()=>{
-        expect(Array.isArray(insertProperties({}, [{}]))).toBe(true);
+        expect(Array.isArray(insertProperties({}, []))).toBe(true);
     });
-    test("host id from singleUserRef (i.e. 1) is returned. Works for one host", ()=>{
+    test("user id is returned correctly for single property data", ()=>{
         const singleUserRef = {"Alice Johnson": 1};
-        const singlePropertyData = [{host_name: "Alice Johnson"}];
+        const singlePropertyData = [
+            {
+                "name": "Modern Apartment in City Center",
+                "property_type": "Apartment",
+                "location": "London, UK",
+                "price_per_night": 120.0,
+                "description": "Description of Modern Apartment in City Center.",
+                "host_name": "Alice Johnson",
+                "amenities": ["WiFi", "TV", "Kitchen"]
+            }
+        ];
 
-        expect((insertProperties(singleUserRef, singlePropertyData))[0][0]).toBe(1);
+        expect(insertProperties(singleUserRef, singlePropertyData)[0][0]).toBe(1);
     }); 
-    test("host ids are returned for > 1 property. Works for multiple hosts.", ()=>{
-        const multiplePropertiesData = [{host_name: "Alice Johnson"}, {host_name: "Emma Davis"}];
-        const output = (insertProperties(multipleUsersRef, multiplePropertiesData));
+    test("user ids are returned correctly for multiple properties data", ()=>{
+        const output = insertProperties(multipleUsersRef, propertyData);
 
         expect(output[0][0]).toBe(1);
         expect(output[1][0]).toBe(2);
@@ -56,7 +76,7 @@ describe("insertProperties", ()=>{
                 "London, UK", 
                 "Apartment", 
                 120.0, 
-                "Description of Modern Apartment in City Center. A sleek apartment with all modern amenities."
+                "Description of Modern Apartment in City Center."
             ], 
             [
                 2, 
@@ -64,19 +84,50 @@ describe("insertProperties", ()=>{
                 "Birmingham, UK", 
                 "Apartment", 
                 110.0, 
-                "Stylish apartment located in the heart of Birmingham, close to all attractions."
+                "Description of Elegant City Apartment."
             ]
         ]);
     });
-    test("output is a nested array", ()=>{
-        const output = insertProperties(multipleUsersRef, propertyData);
+    test("an empty propertyData input returns a new empty array", ()=>{
+        const emptyPropertyData = [];
+        const output = insertProperties(multipleUsersRef, emptyPropertyData);
 
-        output.forEach((nestedItem)=>{
-            expect(Array.isArray(nestedItem)).toBe(true);
-        });
+        expect(output).toEqual([]);
+        expect(output).not.toBe(emptyPropertyData);
     });
-    test("the inputed array is not mutated", ()=>{
-        insertProperties(multipleUsersRef, propertyData)
+    test("an empty userRef input returns an empty array", ()=>{
+        expect(insertProperties({}, propertyData)).toEqual([]);
+    });
+    test("formatted property data is not returned if user (host) name does not exist in userRef", ()=>{
+        const missingUserRef = {"Emma Davis": 2, "Rachel Cummings": 4}; 
+
+        expect(insertProperties(missingUserRef, propertyData)).toEqual(expectedOutcome);
+    });
+    test("formatted property data is not returned if the data has no host_name key", ()=> {
+        const noHostPropertyData = [
+            {
+                "name": "Modern Apartment in City Center",
+                "property_type": "Apartment",
+                "location": "London, UK",
+                "price_per_night": 120.0,
+                "description": "Description of Modern Apartment in City Center.",
+                "amenities": ["WiFi", "TV", "Kitchen"]
+            }, 
+            {
+                "name": "Elegant City Apartment",
+                "property_type": "Apartment",
+                "location": "Birmingham, UK",
+                "price_per_night": 110.0,
+                "description": "Description of Elegant City Apartment.",
+                "host_name": "Emma Davis",
+                "amenities": ["TV", "Kitchen", "Washer"]
+            }
+        ];
+
+        expect(insertProperties(multipleUsersRef, noHostPropertyData)).toEqual(expectedOutcome);
+    });
+    test("the inputted array is not mutated", ()=>{
+        insertProperties(multipleUsersRef, propertyData);
 
         expect(propertyData).toEqual([
             {
@@ -84,29 +135,29 @@ describe("insertProperties", ()=>{
                 "property_type": "Apartment",
                 "location": "London, UK",
                 "price_per_night": 120.0,
-                "description": "Description of Modern Apartment in City Center. A sleek apartment with all modern amenities.",
+                "description": "Description of Modern Apartment in City Center.",
                 "host_name": "Alice Johnson",
-                "amenities": ["WiFi", "TV", "Kitchen", "Washing Machine"]
+                "amenities": ["WiFi", "TV", "Kitchen"]
             },
             {
                 "name": "Elegant City Apartment",
                 "property_type": "Apartment",
                 "location": "Birmingham, UK",
                 "price_per_night": 110.0,
-                "description": "Stylish apartment located in the heart of Birmingham, close to all attractions.",
+                "description": "Description of Elegant City Apartment.",
                 "host_name": "Emma Davis",
-                "amenities": ["WiFi", "TV", "Kitchen", "Washing Machine", "Iron"]
+                "amenities": ["TV", "Kitchen", "Washer"]
             }
         ]);
     });
     test("the inputed object is not mutated", ()=>{
-        insertProperties(multipleUsersRef, propertyData)
+        insertProperties(multipleUsersRef, propertyData);
 
         expect(multipleUsersRef).toEqual({"Alice Johnson": 1, "Emma Davis": 2});
     });
-     test("the output array is a new array", ()=>{
-        const output = insertProperties(multipleUsersRef, propertyData)
+    test("the output array is a new array", ()=>{
+        const output = insertProperties(multipleUsersRef, propertyData);
 
         expect(propertyData).not.toBe(output);
     });
-})
+});
