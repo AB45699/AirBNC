95180be9e92feb96bd7dc9af43396dea57ec2af8
AB45699 <b.anam8541@gmail.com>
2025-12-27 17:57:24 +0000

    refactoring code and including edge case testing

--- db/queries/insert-functions/insertBookings.js
+++ db/queries/insert-functions/insertBookings.js
@@ -1,10 +1,14 @@
 function insertBookings(propertyRef, userRef, bookingsData) {
-    return bookingsData.map(({property_name, guest_name, check_in_date, check_out_date})=>[
-        propertyRef[property_name], 
-        userRef[guest_name], 
-        check_in_date, 
-        check_out_date
-    ])
+    return bookingsData
+        .filter(({ property_name, guest_name }) => 
+            (propertyRef[property_name] !== undefined) && (userRef[guest_name] !== undefined)
+        )
+        .map(({ property_name, guest_name, check_in_date, check_out_date }) => [
+            propertyRef[property_name],
+            userRef[guest_name],
+            check_in_date,
+            check_out_date
+        ]);
 };
 
 module.exports = insertBookings;
--- db/queries/insert-functions/insertBookings.test.js
+++ db/queries/insert-functions/insertBookings.test.js
@@ -1,6 +1,6 @@
 const insertBookings = require("./insertBookings.js"); 
 
-let multiplePropertiesRef, multipleUsersRef, bookingsData; 
+let multiplePropertiesRef, multipleUsersRef, bookingsData, expectedOutcome; 
 
 beforeEach(()=>{
     multiplePropertiesRef = {
@@ -27,27 +27,36 @@ beforeEach(()=>{
             "check_out_date": "2025-03-05"
         }
     ];
-})
+
+    expectedOutcome = [
+        [
+            1,
+            2, 
+            "2025-02-15",
+            "2025-02-20"
+        ]
+    ];
+});
 
 describe("insertBookings", ()=> {
     test("returns an array", ()=>{
         expect(Array.isArray(insertBookings({}, {}, []))).toBe(true);
     }); 
-    test("returns the property id (i.e. 1) for a single property. Works for one property item", ()=>{
+    test("returns property_id and user_id correctly for single booking data", ()=>{
         const singlePropertyRef = {"Modern Apartment in City Center": 1}; 
-        const singleBookingsData = [{"property_name": "Modern Apartment in City Center"}];
-        const output = insertBookings(singlePropertyRef, {}, singleBookingsData);
+        const singleUserRef = {"Bob Smith": 2};
+        const singleBookingsData = [
+            {
+                "property_name": "Modern Apartment in City Center",
+                "guest_name": "Bob Smith"
+            }
+        ];
+        const output = insertBookings(singlePropertyRef, singleUserRef, singleBookingsData);
 
         expect(output[0][0]).toBe(1);
+        expect(output[0][1]).toBe(2);
     });
-    test("returns the user id (i.e. 3) for a single user. Works for one user item", ()=>{
-        const singleUserRef = {"Bob Smith": 3}; 
-        const singleBookingsData = [{"guest_name": "Bob Smith"}];
-        const output = insertBookings({}, singleUserRef, singleBookingsData);
-
-        expect(output[0][1]).toBe(3);
-    });
-    test("property id and user ids are returned for multiple property/user items", ()=>{
+    test("property id and user ids are returned correctly for multiple booking data", ()=>{
         const output = insertBookings(multiplePropertiesRef, multipleUsersRef, bookingsData);
 
         expect(output[0][0]).toBe(1);
@@ -72,12 +81,60 @@ describe("insertBookings", ()=> {
             ]
         ])
     });
-    test("output is a nested array", ()=>{
-        const output = insertBookings(multiplePropertiesRef, multipleUsersRef, bookingsData);
+    test("inputting empty bookings data, returns a new empty array", ()=>{
+        const emptyBookingsData = [];
+        const output = insertBookings(multiplePropertiesRef, multipleUsersRef, emptyBookingsData);
 
-        output.forEach((nestedItem)=>{
-            expect(Array.isArray(nestedItem)).toBe(true);
-        });
+        expect(output).toEqual([]);
+        expect(output).not.toBe(emptyBookingsData);
+    });
+    test("empty ref objects return an empty array", ()=>{
+        expect(insertBookings({}, multipleUsersRef, bookingsData)).toEqual([]);
+        expect(insertBookings(multiplePropertiesRef, {}, bookingsData)).toEqual([]);
+    });
+    test("formatted booking data is not returned for a booking if property name doesn't exist in propertyRef", ()=>{
+        const missingPropertyRef = {"Chic Studio Near the Beach": 9, "Modern Apartment in City Center": 1};
+
+        expect(insertBookings(missingPropertyRef, multipleUsersRef, bookingsData)).toEqual(expectedOutcome);
+    });
+    test("formatted booking data is not returned for a booking if guest name doesn't exist in userRef", ()=>{
+        const missingUserRef = {"Bob Smith": 2, "Rachel Cummings": 6};
+
+        expect(insertBookings(multiplePropertiesRef, missingUserRef, bookingsData)).toEqual(expectedOutcome);
+    });
+    test("formatted booking data is not returned for a booking if booking has no property name key", ()=>{
+        const noPropertyBooking = [
+            {
+                "guest_name": "Frank White",
+                "check_in_date": "2025-03-01",
+                "check_out_date": "2025-03-05"
+            },
+            {
+                "property_name": "Modern Apartment in City Center",
+                "guest_name": "Bob Smith",
+                "check_in_date": "2025-02-15",
+                "check_out_date": "2025-02-20"
+            }
+        ];
+
+        expect(insertBookings(multiplePropertiesRef, multipleUsersRef, noPropertyBooking)).toEqual(expectedOutcome);
+    });
+    test("formatted booking data is not returned for a booking if booking has no guest name key", ()=>{
+        const noGuestBooking = [
+            {
+                "property_name": "Cosy Family House",
+                "check_in_date": "2025-03-01",
+                "check_out_date": "2025-03-05"
+            },
+            {
+                "property_name": "Modern Apartment in City Center",
+                "guest_name": "Bob Smith",
+                "check_in_date": "2025-02-15",
+                "check_out_date": "2025-02-20"
+            }
+        ];
+
+        expect(insertBookings(multiplePropertiesRef, multipleUsersRef, noGuestBooking)).toEqual(expectedOutcome);
     });
     test("the inputted objects are not mutated", ()=>{
         insertBookings(multiplePropertiesRef, multipleUsersRef, bookingsData);
