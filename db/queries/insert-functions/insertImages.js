5b996e5b616eefcfaa4f63cbbbba80fa0fda5099
AB45699 <b.anam8541@gmail.com>
2025-12-27 19:39:31 +0000

    refactoring code, and adding edge cases in tests

--- db/queries/insert-functions/insertImages.js
+++ db/queries/insert-functions/insertImages.js
@@ -1,9 +1,13 @@
 function insertImages(propertyRef, imagesData) {
-    return imagesData.map(({property_name, image_url, alt_tag})=>[
-        propertyRef[property_name],
-        image_url, 
-        alt_tag
-    ])
+    return imagesData
+        .filter(({property_name})=>
+            propertyRef[property_name] !== undefined
+        )
+        .map(({property_name, image_url, alt_tag})=>[
+            propertyRef[property_name],
+            image_url, 
+            alt_tag
+        ])
 };
 
 module.exports = insertImages;
--- db/queries/insert-functions/insertImages.test.js
+++ db/queries/insert-functions/insertImages.test.js
@@ -6,13 +6,13 @@ beforeEach(()=>{
     imagesData = [
         {
             "property_name": "Modern Apartment in City Center",
-            "image_url": "https://images.unsplash.com/photo-1471086569966-db3eebc25a59?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            "alt_tag": "Plant cutting in a clear glass vase filled with water."
+            "image_url": "https://example.com/images/modern_apartment_1.jpg",
+            "alt_tag": "Alt tag for Modern Apartment in City Center"
         },
         {
             "property_name": "Cosy Family House",
-            "image_url": "https://images.unsplash.com/photo-1613545325268-9265e1609167?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            "alt_tag": "Modern minimalist living room with stone fireplace, large windows, and neutral furnishings."
+            "image_url": "https://example.com/images/cosy_family_house_1.jpg",
+            "alt_tag": "Alt tag for Cosy Family House"
         }
     ];
 
@@ -26,15 +26,20 @@ describe("insertImages", ()=>{
     test("returns an array", ()=>{
         expect(Array.isArray(insertImages({}, [{}]))).toBe(true);
     }); 
-    test("the property id from singlePropertyRef (i.e. 1) is returned. Works for a single property", ()=>{
+    test("the property id is returned correctly for single images data", ()=>{
         const singlePropertyRef = {"Modern Apartment in City Center": 1};
-        const singleImagesData = [{property_name: "Modern Apartment in City Center"}];
+        const singleImagesData = [
+            {
+                property_name: "Modern Apartment in City Center",
+                image_url: "https://example.com/images/modern_apartment_1.jpg",
+                alt_tag: "Alt tag for Modern Apartment in City Center"
+            }
+        ];
 
         expect((insertImages(singlePropertyRef, singleImagesData))[0][0]).toBe(1);
     }); 
-    test("the property ids are returned for > 1 property. Works for multiple properties", ()=>{
-        const multipleImagesData = [{property_name: "Modern Apartment in City Center"}, {property_name: "Cosy Family House"}];
-        const output = (insertImages(multiplePropertiesRef, multipleImagesData));
+    test("the property ids are returned correctly for multiple images data", ()=>{
+        const output = (insertImages(multiplePropertiesRef, imagesData));
 
         expect(output[0][0]).toBe(1);
         expect(output[1][0]).toBe(2);
@@ -43,22 +48,57 @@ describe("insertImages", ()=>{
         expect(insertImages(multiplePropertiesRef, imagesData)).toEqual([
             [
                 1,
-                "https://images.unsplash.com/photo-1471086569966-db3eebc25a59?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                "Plant cutting in a clear glass vase filled with water."
+                "https://example.com/images/modern_apartment_1.jpg",
+                "Alt tag for Modern Apartment in City Center"
             ],
             [
                 2,
-                "https://images.unsplash.com/photo-1613545325268-9265e1609167?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                "Modern minimalist living room with stone fireplace, large windows, and neutral furnishings."
+                "https://example.com/images/cosy_family_house_1.jpg",
+                "Alt tag for Cosy Family House"
             ]
         ])
     });
-    test("output is a nested array", ()=>{
-        const output = insertImages(multiplePropertiesRef, imagesData);
+    test("an empty imagesData input returns a new empty array", ()=>{
+        const emptyImagesData = [];
+        const output = insertImages(multiplePropertiesRef, emptyImagesData);
 
-        output.forEach((nestedItem)=>{
-            expect(Array.isArray(nestedItem)).toBe(true);
-        });
+        expect(output).toEqual([]);
+        expect(output).not.toBe(emptyImagesData);
+    });
+    test("empty properties ref object returns empty array", ()=>{
+        expect(insertImages({}, imagesData)).toEqual([]);
+    });
+    test("formatted images data is omitted if property name does not exist in propertyRef", ()=>{
+        const missingPropertyRef = {"Cosy Family House": 2, "Chic Studio Near the Beach": 7};
+
+        expect(insertImages(missingPropertyRef, imagesData)).toEqual([
+            [
+                2,
+                "https://example.com/images/cosy_family_house_1.jpg",
+                "Alt tag for Cosy Family House"
+            ]
+        ])
+    });
+    test("formatted images data is omitted if there is no property_name key in imagesData", ()=>{
+        const noPropertyImagesData = [
+            {
+                "image_url": "https://example.com/images/modern_apartment_1.jpg",
+                "alt_tag": "Alt tag for Modern Apartment in City Center"
+            },
+            {
+                "property_name": "Cosy Family House",
+                "image_url": "https://example.com/images/cosy_family_house_1.jpg",
+                "alt_tag": "Alt tag for Cosy Family House"
+            }
+        ];
+
+        expect(insertImages(multiplePropertiesRef, noPropertyImagesData)).toEqual([
+            [
+                2,
+                "https://example.com/images/cosy_family_house_1.jpg",
+                "Alt tag for Cosy Family House"
+            ]
+        ]);
     });
     test("the inputted array is not mutated", ()=>{
         insertImages(multiplePropertiesRef, imagesData); 
@@ -66,15 +106,15 @@ describe("insertImages", ()=>{
         expect(imagesData).toEqual([
             {
                 "property_name": "Modern Apartment in City Center",
-                "image_url": "https://images.unsplash.com/photo-1471086569966-db3eebc25a59?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                "alt_tag": "Plant cutting in a clear glass vase filled with water."
+                "image_url": "https://example.com/images/modern_apartment_1.jpg",
+                "alt_tag": "Alt tag for Modern Apartment in City Center"
             },
             {
                 "property_name": "Cosy Family House",
-                "image_url": "https://images.unsplash.com/photo-1613545325268-9265e1609167?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                "alt_tag": "Modern minimalist living room with stone fireplace, large windows, and neutral furnishings."
+                "image_url": "https://example.com/images/cosy_family_house_1.jpg",
+                "alt_tag": "Alt tag for Cosy Family House"
             }
-        ])
+        ]);
     });
     test("the inputted object is not mutated", ()=>{
         insertImages(multiplePropertiesRef, imagesData);
